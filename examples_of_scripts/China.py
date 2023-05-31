d054ac1d8a39567249e36f1d9ae9f1a6c7c4102d
bertrand <bertrand.maubert.info@gmail.com>
2023-05-31 23:03:26 +0200

    Amélioration de l'exemple Chine.

--- examples_of_scripts/China.py
+++ examples_of_scripts/China.py
@@ -3,14 +3,21 @@ import matplotlib.pyplot as plt
 
 from pygeomap_src.map_maker import villes_examples
 
+villes_chine = {
+    'Pekin': {},
+    'Canton': {'coord': (23.1301964, 113.2592945)},
+    'Yumen': {},
+    'Ville quelconque': {'coord': (53.5074, -1.2578), 'label': 'E'},
+}
 
-map = Mapper(country='China', title="La chine", points=None)
+map = Mapper(country='China', title="La chine", points=villes_chine, etopo=True)
 
 map.creer_carte()
 #
+
 # map.ajout_regions([('nom', 'Occitanie', 'red', 0.5), ('code_insee', '93', '#FF8957', 0.6)])
 # # '#FF8957'
-# map.dessine_villes()
+map.dessine_villes()
 
 # afficher la carte d'Angleterre
 plt.show()
